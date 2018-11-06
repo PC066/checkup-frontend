85218267df8c77424661fc27872ccebd645ad695
Ryan Hall <hall.ryan.r@gmail.com>
2018-11-05 19:42:35 -0500

    remove routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,11 +1,15 @@
+const API = "http://api.checkup.vip/v1/";
+const PREFIX = process.env.NODE_ENV === "production" ? API : "/";
+import path from "path";
+
 export default {
 
   getQuestionnairePath(digest_key) {
-    return `/user_questionnaires/${digest_key}`;
+    return path.join(PREFIX, "user_questionnaires", digest_key);
   },
 
   postQuestionnairePath(digest_key) {
-    return `/questionnaire_submissions/${digest_key}/submit`;
+    return path.join(PREFIX, "questionnaire_submissions", digest_key, "submit");
   }
-  
+
 }
